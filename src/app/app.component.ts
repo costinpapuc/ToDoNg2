af872267389feb74106c06075c1e2fbb03ef55f8
Costin Papuc <CostinGiorgian.Papuc@endava.com>
2016-11-08 14:15:53 +0200

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Type } from '@angular/core';
+import { TodoAppComponent } from './todo-app/todo-app.component';
 
 @Component({
   selector: 'app-root',
